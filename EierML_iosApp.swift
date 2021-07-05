a6426cf5b012662831616ffbfcbcfb1fb5b5fe44
Torben Köhler <t.koehler@schueler.lg-ks.de>
2021-07-05 16:41:16 +0200

    Initial commit

---
+++ EierML_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  EierML_iosApp.swift
+//  EierML-ios
+//
+//  Created by Torben Köhler on 05.07.21.
+//
+
+import SwiftUI
+
+@main
+struct EierML_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
