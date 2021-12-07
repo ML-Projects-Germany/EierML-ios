2f819a64140aedab1a9e5cfac426637c13ea4b5d
Torben Köhler <t.koehler@schueler.lg-ks.de>
2021-12-07 23:14:42 +0100

    Add EggCardView

---
+++ EierML-ios/Views/EierML/TimerView/EggCardView.swift
@@ -0,0 +1,20 @@
+//
+//  EggCardView.swift
+//  EierML-ios
+//
+//  Created by Torben Köhler on 07.12.21.
+//
+
+import SwiftUI
+
+struct EggCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EggCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        EggCardView()
+    }
+}
